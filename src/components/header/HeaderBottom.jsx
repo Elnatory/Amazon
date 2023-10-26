3a1a48e57f8779b186a9a99198658820784143a3
Muhammad Omar <m.elnatory@gmail.com>
2023-10-26 19:20:14 +0300

    Make Header & Bottom Header & SideBar

--- src/components/header/HeaderBottom.jsx
+++ src/components/header/HeaderBottom.jsx
@@ -4,6 +4,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { useSelector } from "react-redux";
+import SideNavContent from "./SideHeader";
 
 export default function HeaderBottom(props) {
     const [sidebar, setSidebar] = useState(false);
@@ -26,7 +27,55 @@ export default function HeaderBottom(props) {
                     <li className="hidden md:inline-flex headerHover">Registry</li>
                     <li className="hidden md:inline-flex headerHover">Sell</li>
                 </ul>
-            </div>
+                {/* ======================= List Items End here ========================== */}
+                {/* ======================= SideBar Start here =========================== */}
+                {sidebar && (
+                    <div className="w-full h-screen text-black fixed top-0 left-0 bg-amazon_blue bg-opacity-50">
+                        <div className="w-full h-full relative">
+                            <div className="w-[350px] h-full bg-white border border-black">
+                                <div className="w-full bg-amazon_light text-white py-2 px-6 flex items-center gap-4">
+                                    <AccountCircleIcon />
+                                    <h3 className="font-titleFont font-bold text-lg tracking-wide">
+                                        Hello, Sign In
+                                    </h3>
+                                </div>
+                                {/* ============================ Content & Devices Start here ================ */}
+                                <SideNavContent
+                                    title="Digital Content & Devices"
+                                    one="Amazon Music"
+                                    two="Kindle E-readers & Books"
+                                    three="Amazon Appstore"
+                                />
+                                <SideNavContent
+                                    title="Shop By Department"
+                                    one="Electronics"
+                                    two="Computers"
+                                    three="Smart Home"
+                                />
+                                <SideNavContent
+                                    title="Programs & Features"
+                                    one="Gift Cards"
+                                    two="Amazon live"
+                                    three="International Shopping"
+                                />
+                                <SideNavContent
+                                    title="Help & Settings"
+                                    one="Your Account"
+                                    two="Customer Service"
+                                    three="Contact us"
+                                />
+                                {/* ============================ Content & Devices End here ================ */}
+                                <span
+                                    onClick={() => setSidebar(false)}
+                                    className="cursor-pointer absolute top-0 left-[300px] md:left-[360px] w-10 h-10 text-black flex items-center justify-center border bg-gray-200 hover:bg-red-500 hover:text-white duration-300"
+                                >
+                                    <CloseIcon />
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div >
         </>
     )
 }
