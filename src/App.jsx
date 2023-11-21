5ebe7eed856a72bc62550af5d46df55b386daa70
Muhammad Omar <m.elnatory@gmail.com>
2023-11-21 04:28:49 +0200

    handle Muhammad

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css'
-import React from 'react'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
-import Products from './components/home/Products'
+import "./App.css";
+import React from "react";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Products from "./components/home/Products";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -11,20 +11,21 @@ import {
   RouterProvider,
   ScrollRestoration,
 } from "react-router-dom";
-import Home from './pages/Home'
-import Cart from './pages/Cart';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import CheckOut from './pages/Checkout';
-import Orders from './pages/Orders'
-import Brands from './pages/Brands';
-import Categories from './pages/Category'
-import NotFound from './pages/notFound/NotFound'
-import SubCategory from './pages/Sub-Category'
-import Details from './pages/details/Details'
-import Help from './pages/Help';
-import { Provider } from 'react-redux';
-import store from './store/store';
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import CheckOut from "./pages/Checkout";
+import Orders from "./pages/Orders";
+import Brands from "./pages/Brands";
+import Categories from "./pages/Category";
+import NotFound from "./pages/notFound/NotFound";
+import SubCategory from "./pages/Sub-Category";
+import Details from "./pages/details/Details";
+import Help from "./pages/Help";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import BrandsDetails from "./pages/BrandsDetails";
 
 const Layout = () => {
   return (
@@ -44,11 +45,15 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
-          <Route path="/:catName" element={<Categories />}></Route>
-          <Route path='/:catName/:sub' element={<SubCategory />}></Route>
-          <Route path='/details/:id/:sim?' element={<Details />}></Route>
+          <Route path="/categories" element={<Categories />}></Route>
+          <Route
+            path="categories/:catName/:sub"
+            element={<SubCategory />}
+          ></Route>
+          <Route path="/details/:id/:sim?" element={<Details />}></Route>
           <Route path="/orders" element={<Orders />}></Route>
           <Route path="/brands" element={<Brands />}></Route>
+          <Route path="/brands/:brandSlug" element={<BrandsDetails />}></Route>
           <Route path="/help" element={<Help />}></Route>
         </Route>
         <Route path="/signin" element={<Signin />}></Route>
@@ -67,5 +72,4 @@ function App() {
   );
 }
 
-export default App
-
+export default App;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,7 +10,7 @@ import {
 import { motion } from "framer-motion";
 import { emptyCart } from "../assets";
 import { Link } from "react-router-dom";
-import Slides from '../components/slides/slides'
+import Slides from "../components/slides/slides";
 
 const Cart = () => {
   const products = useSelector((state) => state.amazonReducer.products);
@@ -28,105 +28,114 @@ const Cart = () => {
     <div className="w-full bg-gray-100 p-3">
       {products.length > 0 ? (
         <>
-        <div className="h-auto grid grid-cols-5 gap-8 mx-auto">
-          <div className="w-full bg-white px-4 col-span-5 xl:col-span-4">
-            {/* ====================== Cart title Start here ======================= */}
-            <div className="font-titleFont hidden xl:flex items-center justify-between py-3">
-              <h1 className="text-3xl">Shopping Cart</h1>
-              <h6 className="text-xl self-end">Price</h6>
-            </div>
-            <p className="text-sm text-gray-500 border-b-[1px]  border-b-gray-400">Deselected items</p>
-            {/* ====================== Cart title End here ========================= */}
-            {/* ====================== Products Start here ========================= */}
-            <div>
-              {products.map((item) => (
-                <div
-                  key={item.id}
-                  className="w-full border-b-[1px] border-b-gray-300 p-4 md:p-0 md:py-4 flex items-center gap-6"
-                >
-                  <div className="w-full flex flex-col md:flex-row items-center gap-6">
-                    {/* ====================== Left Start here ============================ */}
-                    <div className="w-full md:w-2/5 xl:w-1/5">
-                      <img
-                        className="w-full h-44 object-contain"
-                        src={item.image}
-                        alt="productImg"
-                      />
-                    </div>
-                    <div className="w-full flex flex-col gap-2 xl:gap-1">
-                      <h2 className="font-semibold text-lg">{item.title}</h2>
-                      <p className="xl:pr-10 text-sm">{item.description}</p>
-                      <p className="text-base">
-                        Unit Price:{" "}
-                        <span className="font-semibold">EGP {item.price}.00</span>
-                      </p>
-                      <div className="bg-[#F0F2F2] flex justify-center items-center gap-2 w-36 py-1 text-center drop-shadow-lg rounded-md">
-                        <p className="text-base font-normal">Qty:</p>
-                        <p
-                          onClick={() => {
-                            dispatch(decreaseQuantity(item.id));
-                          }}
-                          className="cursor-pointer bg-gray-200 px-2 rounded-sm hover:bg-gray-400 font-semibold duration-300"
-                        >
-                          -
+          <div className="h-auto grid grid-cols-5 gap-8 mx-auto">
+            <div className="w-full bg-white px-4 col-span-5 xl:col-span-4">
+              {/* ====================== Cart title Start here ======================= */}
+              <div className="font-titleFont hidden xl:flex items-center justify-between py-3">
+                <h1 className="text-3xl">Shopping Cart</h1>
+                <h6 className="text-xl self-end">Price</h6>
+              </div>
+              <p className="text-sm text-gray-500 border-b-[1px]  border-b-gray-400">
+                Deselected items
+              </p>
+              {/* ====================== Cart title End here ========================= */}
+              {/* ====================== Products Start here ========================= */}
+              <div>
+                {products.map((item) => (
+                  <div
+                    key={item.id}
+                    className="w-full border-b-[1px] border-b-gray-300 p-4 md:p-0 md:py-4 flex items-center gap-6"
+                  >
+                    <div className="w-full flex flex-col md:flex-row items-center gap-6">
+                      {/* ====================== Left Start here ============================ */}
+                      <div className="w-full md:w-2/5 xl:w-1/5">
+                        <img
+                          className="w-full h-44 object-contain"
+                          src={item.image}
+                          alt="productImg"
+                        />
+                      </div>
+                      <div className="w-full flex flex-col gap-2 xl:gap-1">
+                        <h2 className="font-semibold text-lg">{item.title}</h2>
+                        <p className="xl:pr-10 text-sm">{item.description}</p>
+                        <p className="text-base">
+                          Unit Price:{" "}
+                          <span className="font-semibold">
+                            EGP {item.price}.00
+                          </span>
                         </p>
-                        <p className="font-titleFont text-base font-semibold text-amazon_blue">
-                          {item.quantity}
-                        </p>
-                        <p
-                          onClick={() => dispatch(increaseQuantity(item.id))}
-                          className="cursor-pointer bg-gray-200 px-2 rounded-sm hover:bg-gray-400 font-semibold duration-300"
+                        <div className="bg-[#F0F2F2] flex justify-center items-center gap-2 w-36 py-1 text-center drop-shadow-lg rounded-md">
+                          <p className="text-base font-normal">Qty:</p>
+                          <p
+                            onClick={() => {
+                              dispatch(decreaseQuantity(item.id));
+                            }}
+                            className="cursor-pointer bg-gray-200 px-2 rounded-sm hover:bg-gray-400 font-semibold duration-300"
+                          >
+                            -
+                          </p>
+                          <p className="font-titleFont text-base font-semibold text-amazon_blue">
+                            {item.quantity}
+                          </p>
+                          <p
+                            onClick={() => dispatch(increaseQuantity(item.id))}
+                            className="cursor-pointer bg-gray-200 px-2 rounded-sm hover:bg-gray-400 font-semibold duration-300"
+                          >
+                            +
+                          </p>
+                        </div>
+                        <button
+                          onClick={() => dispatch(deleteItem(item.id))}
+                          className="bg-red-500 w-36 py-1 rounded-lg text-white mt-2 hover:bg-red-700 active:bg-red-900 duration-300"
                         >
-                          +
+                          Delete Item
+                        </button>
+                      </div>
+                      {/* ====================== Left End here ============================== */}
+                      {/* ====================== Right Start here =========================== */}
+
+                      <div className="w-full md:w-24">
+                        <p className="text-lg xl:w-24 font-titleFont font-semibold">
+                          EGP {item.price * item.quantity}
                         </p>
                       </div>
-                      <button
-                        onClick={() => dispatch(deleteItem(item.id))}
-                        className="bg-red-500 w-36 py-1 rounded-lg text-white mt-2 hover:bg-red-700 active:bg-red-900 duration-300"
-                      >
-                        Delete Item
-                      </button>
+                      {/* ====================== Right End here ============================= */}
                     </div>
-                    {/* ====================== Left End here ============================== */}
-                    {/* ====================== Right Start here =========================== */}
-
-                    <div className="w-full md:w-24">
-                      <p className="text-lg xl:w-24 font-titleFont font-semibold">
-                      EGP {item.price * item.quantity}
-                      </p>
-                    </div>
-                    {/* ====================== Right End here ============================= */}
                   </div>
-                </div>
-              ))}
-            </div>
-            {/* ====================== Products End here =========================== */}
-            <div onClick={() => dispatch(resetCart())} className="w-full py-4">
-              <button className="px-10 py-2 bg-red-500 hover:bg-red-600 active:bg-red-500 text-white rounded-lg font-titleFont font-semibold text-lg tracking-wide">
-                Clear Cart
-              </button>
-            </div>
-          </div>
-          <div className="col-span-5 md:col-span-3 lg:col-span-2 xl:col-span-1 mx-auto bg-white h-52 flex items-center p-4">
-            <div>
-              <p className="flex gap-2 items-start text-sm">
-                <span>
-                  <CheckCircleIcon className="bg-white text-green-500 rounded-full" />
-                </span>
-                Your order qualifies for FREE Shipping Choose this option at checkout. See details....
-              </p>
-              <div>
-                <p className="font-semibold px-6 py-1 flex items-center justify-between">
-                Subtotal: <span className="text-lg font-bold">EGP {totalAmt}</span>
-                </p>
+                ))}
               </div>
-              <button className="w-full font-titleFont font-medium text-base bg-gradient-to-tr from-yellow-400 to-yellow-200 border hover:from-yellow-300 hover:to-yellow-400 border-yellow-500 hover:border-yellow-700 active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200 py-1.5 rounded-md mt-3">
-                Proceed to Buy
-              </button>
+              {/* ====================== Products End here =========================== */}
+              <div
+                onClick={() => dispatch(resetCart())}
+                className="w-full py-4"
+              >
+                <button className="px-10 py-2 bg-red-500 hover:bg-red-600 active:bg-red-500 text-white rounded-lg font-titleFont font-semibold text-lg tracking-wide">
+                  Clear Cart
+                </button>
+              </div>
+            </div>
+            <div className="col-span-5 md:col-span-3 lg:col-span-2 xl:col-span-1 mx-auto bg-white h-52 flex items-center p-4">
+              <div>
+                <p className="flex gap-2 items-start text-sm">
+                  <span>
+                    <CheckCircleIcon className="bg-white text-green-500 rounded-full" />
+                  </span>
+                  Your order qualifies for FREE Shipping Choose this option at
+                  checkout. See details....
+                </p>
+                <div>
+                  <p className="font-semibold px-6 py-1 flex items-center justify-between">
+                    Subtotal:{" "}
+                    <span className="text-lg font-bold">EGP {totalAmt}</span>
+                  </p>
+                </div>
+                <button className="w-full font-titleFont font-medium text-base bg-gradient-to-tr from-yellow-400 to-yellow-200 border hover:from-yellow-300 hover:to-yellow-400 border-yellow-500 hover:border-yellow-700 active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200 py-1.5 rounded-md mt-3">
+                  Proceed to Buy
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <Slides/>
+          <Slides />
         </>
       ) : (
         <motion.div
