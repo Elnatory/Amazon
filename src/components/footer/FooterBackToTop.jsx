bf1a234a0c570e6e3e0a6202f004fe57f439a6c6
Muhammad Omar <m.elnatory@gmail.com>
2023-11-27 07:52:06 +0200

    Muhammad LazyLoading

---
+++ src/components/footer/FooterBackToTop.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const FooterBackToTop = () => {
+  const handleBackToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <div
+      className="w-full bg-gray-700 cursor-pointer text-gray-100 text-white transition duration-300 hover:bg-gray-600"
+      onMouseOver={() => {}}
+      onMouseOut={() => {}}
+      onClick={handleBackToTop}
+    >
+      <div className="w-full py-5">
+        <div className="max-w-5xl mx-auto flex gap-1 items-center justify-center">
+          <p className="font-normal text-[13px] cursor-pointer leading-3">
+            Back to top
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterBackToTop;
