4371f1347cdce56c3838996b8b83717d822ecace
fadyshenoda090 <fady.shenouda@com.svu.edu.eg>
2023-12-06 18:32:20 +0200

    fixing singIn and localStorage user id and payment form condition is correct

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,12 +1,12 @@
-import React, { useContext,useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import { darkLogo, AmazonEgypt } from "../assets/index";
 import * as EmailValidator from "email-validator";
 import passwordRegexp from "password-regexp";
 import { useNavigate } from "react-router-dom";
-import {login} from "../firebase/auth"
-import toast,{ Toaster } from 'react-hot-toast';
+import { login } from "../firebase/auth"
+import toast, { Toaster } from 'react-hot-toast';
 import { authContext } from "../Contexts/isAuth";
 
 
@@ -15,82 +15,78 @@ export default function Signin(props) {
 
 
     // const {setLogin,setDisplayName }=useContext(authContext)
-    const {setLogin,setDisplayRes }=useContext(authContext)
+    const { setLogin, setDisplayRes } = useContext(authContext)
     const navigate = useNavigate();
     const [user, setUser] = useState({
-      email: "",
-      password: "",
+        email: "",
+        password: "",
     });
-  
+
     const [errors, setErrors] = useState({
-      emailError: "",
-      passwordError: "",
+        emailError: "",
+        passwordError: "",
     });
-  
+
     const handleForm = (evt) => {
-    //   const { name, value } = evt.target;
-  
-     if (evt.target.name === "email") {
-        setUser({ ...user, email: evt.target.value });
-        setErrors({
-          ...errors,
-          emailError:
-          evt.target.value.length === 0
-              ? "Email is required"
-              : EmailValidator.validate(evt.target.value)
-              ? ""
-              : "Please Enter a Valid Email",
-        });
-      }
-      else if (evt.target.name === "password") {
-        setUser({ ...user, password: evt.target.value });
-        setErrors({
-          ...errors,
-          passwordError:
-          evt.target.value.length === 0
-              ? "Password is Required"
-              : passwordRegexp().test(evt.target.value)
-              ? ""
-              : "Password must be at least 3 characters",
-        });
-      }
+        //   const { name, value } = evt.target;
+
+        if (evt.target.name === "email") {
+            setUser({ ...user, email: evt.target.value });
+            setErrors({
+                ...errors,
+                emailError:
+                    evt.target.value.length === 0
+                        ? "Email is required"
+                        : EmailValidator.validate(evt.target.value)
+                            ? ""
+                            : "Please Enter a Valid Email",
+            });
+        }
+        else if (evt.target.name === "password") {
+            setUser({ ...user, password: evt.target.value });
+            setErrors({
+                ...errors,
+                passwordError:
+                    evt.target.value.length === 0
+                        ? "Password is Required"
+                        : passwordRegexp().test(evt.target.value)
+                            ? ""
+                            : "Password must be at least 3 characters",
+            });
+        }
     };
-  
+
     const signinhandle = async (e) => {
-      e.preventDefault();
-      try{
-        if (errors.emailError||errors.passwordError) {
-  
-        toast.error("write a Valid Email or password")
-  
-      }else{
-        const res= await login(user.email,user.password)
-        console.log(res)
-        localStorage.setItem('token',res.user.accessToken) 
-        localStorage.setItem('displayName', res.user.displayName);
-        localStorage.setItem('email', res.user.email);
-        localStorage.setItem('creatAt', res.user.metadata.creationTime);
-        localStorage.setItem('user', res.user.uid);
+        e.preventDefault();
+        try {
+            if (errors.emailError || errors.passwordError) {
 
+                toast.error("write a Valid Email or password")
 
+            } else {
+                const retrievedData = await login(user.email, user.password)
+                console.log(retrievedData)
+                localStorage.setItem('token', retrievedData.user.accessToken)
+                localStorage.setItem('displayName', retrievedData.user.displayName);
+                localStorage.setItem('email', retrievedData.user.email);
+                localStorage.setItem('creatAt', retrievedData.user.metadata.creationTime);
+                localStorage.setItem('user', retrievedData.user.uid);
+                setLogin(true)
+                navigate("/")
 
-
-        setLogin(true)
-         navigate("/")
-  
-      }
-      }catch(err){
-        toast.error(" some thing is wrong ")
-    }
+            }
+        } catch (err) {
+            toast.error(" some thing is wrong ")
+        }
     };
 
     return (
         <>
-            <div className="w-full"  onSubmit={signinhandle}>
+            <div className="w-full" onSubmit={signinhandle}>
                 <div className="w-full bg-white pb-10">
                     <form className="w-[350px] mx-auto flex flex-col items-center">
                         <Link to="/">
-                            <img className="w-32" src={darkLogo} alt="darkLogo"/>
+                            <img className="w-32" src={darkLogo} alt="darkLogo" />
                         </Link>
                         <div className="w-full border border-zinc-200 p-6">
                             <h2 className="font-titleFont text-3xl font-medium mb-4">
@@ -102,14 +98,14 @@ export default function Signin(props) {
                                         Email or mobile phone number
                                     </label>
                                     <input
-                                     name="email"
-                                     value={user.email}
-                                     onChange={handleForm}
+                                        name="email"
+                                        value={user.email}
+                                        onChange={handleForm}
                                         type="email"
                                         id="email"
                                         className="w-full lowercase py-1 border border-zinc-400 px-2 text-base rounded-sm outline-none focus-within:border-[#e77600] focus-within:shadow-amazonInput duration-100"
                                     />
-                                              <p style={{ color: "red" }}>{errors.emailError}</p>
+                                    <p style={{ color: "red" }}>{errors.emailError}</p>
 
                                 </div>
                                 <div className="flex flex-col gap-2">
@@ -117,9 +113,9 @@ export default function Signin(props) {
                                         Password
                                     </label>
                                     <input
-                                    name="password"
-                                    value={user.password}
-                                    onChange={handleForm}
+                                        name="password"
+                                        value={user.password}
+                                        onChange={handleForm}
                                         type="password"
                                         id="password"
                                         className="w-full lowercase py-1 border border-zinc-400 px-2 text-base rounded-sm outline-none focus-within:border-[#e77600] focus-within:shadow-amazonInput duration-100"
@@ -150,8 +146,8 @@ export default function Signin(props) {
                         </p>
                         <Link className="w-full" to="/register">
                             <button className="w-full py-1.5 mt-4 text-sm font-normal rounded-sm bg-gradient-to-t from-slate-200 to-slate-100 hover:bg-gradient-to-b border border-zinc-400 active:border-yellow-800 active:shadow-amazonInput" onClick={() => {
-            navigate(`/register`);
-          }}>
+                                navigate(`/register`);
+                            }}>
                                 Create your Amazon account
                             </button>
                         </Link>
@@ -174,7 +170,7 @@ export default function Signin(props) {
                     </p>
                 </div>
             </div>
-            <Toaster  position ="top-center" /> 
+            <Toaster position="top-center" />
         </>
 
     )
